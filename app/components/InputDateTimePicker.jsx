1de68173ff13dd37bfe9d028eca7529916f92001
Yoann Bourdet <ybourdet.trepia@tf1.fr>
2016-06-13 14:34:08 +0200

    add webpack dev server

---
+++ app/components/InputDateTimePicker.jsx
@@ -0,0 +1,29 @@
+'use strict';
+
+import React, { Component, PropTypes } from 'react';
+
+export default class InputDateTimePicker extends Component {
+
+  static propTypes = {
+    placeholder: PropTypes.string,
+  }
+
+  static defaultProps = {
+    placeholder: 'choose your Date',
+  }
+
+  handleFocus() {
+    console.log('okokkokkos');
+  }
+
+  render() {
+    const { placeholder } = this.props;
+    return (
+      <input
+        onFocus={this.handleFocus.bind(this)}
+        placeholder={placeholder}
+        type="text"
+      />
+    );
+  }
+}
--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,26 +1,34 @@
 /* eslint quote-props: 0 */
+import webpack from 'webpack';
 import path from 'path';
 
 module.exports = {
   context: __dirname,
-  entry: {
-    'example/js/bundle/DateTimePicker': './example/js/app.js',
-  },
+  entry: [
+    'webpack-dev-server/client?http://localhost:8080/',
+    'webpack/hot/dev-server',
+    './example/js/app.js',
+  ],
   output: {
-    path: __dirname,
-    filename: '[name].js',
+    path: path.join(__dirname, '/example/js/bundle/'),
+    filename: 'DateTimePicker.js',
+    publicPath: '/static/',
   },
   module: {
     loaders: [{
       test: /\.(js|jsx)$/,
       exclude: /node_modules/,
-      loaders: ['babel'],
+      loaders: ['react-hot', 'babel'],
     }],
   },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+  ],
   resolve: {
     extensions: ['', '.js', '.jsx'],
     alias: {
       DateTimePicker: path.join(__dirname, '/app/components/DateTimePicker'),
+      InputDateTimePicker: path.join(__dirname, '/app/components/InputDateTimePicker'),
     },
   },
   externals: {
